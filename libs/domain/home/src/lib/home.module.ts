6173f9f037a86eec66fd6f187509bd3bf36cd7e7
Victor Piella <victor.piella@quobis.com>
2021-06-14 12:50:06 +0200

    1.3 CodeLab async onpush

--- libs/domain/home/src/lib/home.module.ts
+++ libs/domain/home/src/lib/home.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { HomePage } from './home.page';
-import { CategoryList } from './category-list/category-list.component';
 import { UiModule } from '@ab/ui';
-
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CategoryList } from './category-list/category-list.component';
+import { HomePage } from './home.page';
 @NgModule({
   imports: [
     CommonModule,
--- libs/domain/home/src/lib/home.page.ts
+++ libs/domain/home/src/lib/home.page.ts
@@ -1,5 +1,7 @@
 import { Header } from '@ab/ui';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HomeService } from './home.service';
 import { Category } from './models/category';
 
 @Component({
@@ -8,24 +10,11 @@ import { Category } from './models/category';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
-
-
-  categories: Category[] = [
-    {
-      id: '1',
-      name: 'One',
-      description: 'first'
-    },
-    {
-      id: '1',
-      name: 'One',
-      description: 'first'
-    }
-  ];
-
+  public categories$: Observable<Category[]> = this.service.getCategories$();
   header: Header = {
     heroClass: 'is-primary',
     title: 'The home of the Angular Builders',
     subtitle: 'A site to help you build great applications with Angular',
   };
+  constructor(private service: HomeService) {}
 }
