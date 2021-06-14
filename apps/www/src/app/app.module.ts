f1f1f6f737ab5cf520d07208bb5a7d19fb3f5b8a
Victor Piella <victor.piella@quobis.com>
2021-06-14 14:17:40 +0200

    wip

--- apps/www/src/app/app.module.ts
+++ apps/www/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { LayoutModule } from '@ab/layout';
+import { SearchBoxModule } from '@ab/search-box';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { LayoutModule } from '@ab/layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, LayoutModule, RouterModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    LayoutModule,
+    RouterModule,
+    SearchBoxModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,3 +1,4 @@
-export * from './lib/ui.module';
-export * from './lib/models/header';
 export * from './lib/models/card';
+export * from './lib/models/header';
+export * from './lib/models/notification';
+export * from './lib/ui.module';
