f5bac7063f223e567d9f03a207672e28371c93e5
Victor Piella <victor.piella@quobis.com>
2021-06-14 17:08:12 +0200

    2.3 CodeLab router events

--- apps/www/src/app/core/core.module.ts
+++ apps/www/src/app/core/core.module.ts
@@ -1,15 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 import { CoreRoutingModule } from './core-routing.module';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    CoreRoutingModule,
-    HttpClientModule
-  ]
+  imports: [CommonModule, CoreRoutingModule, HttpClientModule],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(router: Router) {
+    router.events
+      .pipe(
+        filter((routerEvent) => routerEvent instanceof NavigationEnd),
+        map((routerEvent) => routerEvent as NavigationEnd)
+      )
+      .subscribe({
+        next: (navigation: NavigationEnd) =>
+          console.log({
+            category: 'BUSINESS',
+            event: 'NAV',
+            label: navigation.urlAfterRedirects,
+          }),
+      });
+  }
+}
